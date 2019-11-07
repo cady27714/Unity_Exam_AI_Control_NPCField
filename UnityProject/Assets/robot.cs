4dff35c5a066a292f32f246c9513ae6612d71c24
cady27714 <54615342+cady27714@users.noreply.github.com>
2019-11-07 11:51:32 +0800

    1107

---
+++ UnityProject/Assets/robot.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class robot : MonoBehaviour
+{
+    public int speed = 20;
+    public float jump = 2.0f;
+    public string robotName = "機器人";
+    public bool isground = false;
+
+    private Rigidbody2D r2d;
+
+    private void Start()
+    {
+        r2d = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D)) transform.eulerAngles = new Vector3(0, 0, 0);
+        if (Input.GetKeyDown(KeyCode.A)) transform.eulerAngles = new Vector3(0, 180, 0);
+    }
+    private void FixedUpdate()
+    {
+        Walk();
+    }
+
+    //<summary>
+    //走路
+    //</summary>
+
+    private void Walk()
+    {
+        r2d.AddForce(new Vector2(speed * Input.GetAxis("走路"), 0));
+    }
+    }
---
+++ UnityProject/Assets/robot1.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class robot1 : MonoBehaviour
+{
+    public string say = "Hi";
+    public int sayspeed = 20;
+    public bool complete = false;
+    public int BateryFinish;
+    public int Batery;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
